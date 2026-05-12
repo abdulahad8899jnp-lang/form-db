ffce97101325c21a470b1e7e9d3017c016431f00
Abdul Ahad <your-email@gmail.com>
2026-05-12 07:47:29 +0530

    first upload

---
+++ src/App.jsx
@@ -0,0 +1,78 @@
+import { useState } from "react";
+
+function App() {
+
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+
+  const saveData = async (e) => {
+
+    e.preventDefault();
+
+    try {
+
+      const res = await fetch(
+        "http://localhost:5000/save",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type":
+              "application/json",
+          },
+          body: JSON.stringify({
+            name,
+            email,
+          }),
+        }
+      );
+
+      const data = await res.text();
+
+      alert(data);
+
+      setName("");
+      setEmail("");
+
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <div style={{ padding: "40px" }}>
+
+      <form onSubmit={saveData}>
+
+        <input
+          type="text"
+          placeholder="Name"
+          value={name}
+          onChange={(e) =>
+            setName(e.target.value)
+          }
+        />
+
+        <br /><br />
+
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) =>
+            setEmail(e.target.value)
+          }
+        />
+
+        <br /><br />
+
+        <button type="submit">
+          Submit
+        </button>
+
+      </form>
+
+    </div>
+  );
+}
+
+export default App;
